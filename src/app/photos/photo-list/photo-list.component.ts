edd3e9b14d9edba6c12b8cc9bc016db97faef0a3
MonteiroPena <dmpassis@gmail.com>
2020-06-19 18:32:58 -0300

    Binding de eventos, Pipe e implementações, Resolvers, RxjS e seu Subject, Paginação de dados e LoadButton

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+
+import { Photo } from "../photo/photo";
 import { PhotoService } from "../photo/photo.service";
-import { Photo } from '../photo/photo';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -9,20 +12,41 @@ import { Photo } from '../photo/photo';
   templateUrl: "./photo-list.component.html",
   styleUrls: ["./photo-list.component.css"],
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
+  filter = "";
+  debounce: Subject<string> = new Subject<string>();
+  hasMore = true;
+  currentPage: number = 1;
+  userName: string = "";
 
   constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
   ) {}
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data["photos"];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe((filter) => (this.filter = filter));
+  }
+  /*
+  Ele faz parte do ciclo de vida de um componente do Angular, sendo chamado toda vez que um objeto é destruído.
+  Significa que quando sairmos de PhotoListComponent, e ele for destruído, o método será chamado, e faremos o unsubscribe() */
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 
-    this.photoService.listFromUser(userName).subscribe(
-      (photos) => (this.photos = photos),
-      (err) => console.log(err.message)
-    );
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe((photos) => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
   }
 }
