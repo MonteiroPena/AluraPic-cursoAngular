647d001d43fe59d2f1dfb97acb7d2cc0fcdbfa9d
MonteiroPena <dmpassis@gmail.com>
2020-06-30 20:16:44 -0300

    Rotas parametrizadas, parametros de rotas e busca de foto byId da API

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -10,6 +10,7 @@ import { PhotoModule } from "../photo/photo.module";
 import { CardModule } from "../../shared/components/card/card.module";
 import { SearchComponent } from "./search/search.component";
 import { DarkenOnHoverModule } from "src/app/shared/directives/darken-on-hover/darken-on-hover.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { DarkenOnHoverModule } from "src/app/shared/directives/darken-on-hover/d
     PhotoModule,
     CardModule,
     DarkenOnHoverModule,
+    RouterModule,
   ],
 })
 export class PhotoListModule {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 
 import { PhotoModule } from "./photo/photo.module";
 import { PhotoListModule } from "./photo-list/photo-list.module";
 import { PhotoFormModule } from "./photo-form/photo-form.module";
+import { PhotoDetailsModule } from "./photo-details/photo-details.module";
 
 @NgModule({
   imports: [
-      PhotoModule,
-      PhotoFormModule,
-      PhotoListModule,
-      HttpClientModule,
-      CommonModule
-  ]
+    PhotoModule,
+    PhotoFormModule,
+    PhotoListModule,
+    HttpClientModule,
+    CommonModule,
+    PhotoDetailsModule,
+  ],
 })
 export class PhotosModule {}
