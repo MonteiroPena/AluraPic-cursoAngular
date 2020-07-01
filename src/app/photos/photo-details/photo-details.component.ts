2f83ca014638eb16abef7b1c8bf945a4acc2fb3a
MonteiroPena <dmpassis@gmail.com>
2020-07-01 15:54:56 -0300

    Comentando as fotos

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from "@angular/core";
 import { PhotoService } from "../photo/photo.service";
 import { Photo } from "../photo/photo";
 import { Observable } from "rxjs";
+import { PhotoComment } from "../photo/photo-comment";
 
 @Component({
   templateUrl: "./photo-details.component.html",
-  styleUrls: ["photo-details.css"],
 })
 export class PhotoDetailsComponent implements OnInit {
   photo$: Observable<Photo>;
+  photoId: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -17,7 +18,7 @@ export class PhotoDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params.photoId;
-    this.photo$ = this.photoService.findById(id);
+    this.photoId = this.route.snapshot.params.photoId;
+    this.photo$ = this.photoService.findById(this.photoId);
   }
 }
--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,12 +1,22 @@
+import { RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { PhotoDetailsComponent } from "./photo-details.component";
 import { PhotoModule } from "../photo/photo.module";
+import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
+import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 
 @NgModule({
-  declarations: [PhotoDetailsComponent],
-  exports: [PhotoDetailsComponent],
-  imports: [CommonModule, PhotoModule],
+  declarations: [PhotoDetailsComponent, PhotoCommentsComponent],
+  exports: [PhotoDetailsComponent, PhotoCommentsComponent],
+  imports: [
+    CommonModule,
+    PhotoModule,
+    RouterModule,
+    ReactiveFormsModule,
+    VMessageModule,
+  ],
 })
 export class PhotoDetailsModule {}
