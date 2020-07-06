53310d28849dff9338165132999e34c74938b279
MonteiroPena <dmpassis@gmail.com>
2020-07-06 17:17:48 -0300

    Global error handler e log no server side

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from "@angular/router";
+import { NgModule, ErrorHandler } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { GlobalErrorComponent } from "./global-error/global-error.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { GlobalErrorHandle } from "./global-error-handler/global-error-handler";
 
 @NgModule({
-  imports: [
-    CommonModule
+  imports: [CommonModule, RouterModule],
+  declarations: [NotFoundComponent, GlobalErrorComponent],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandle,
+    },
   ],
-  declarations: [NotFoundComponent]
 })
-export class ErrorsModule { }
+export class ErrorsModule {}
