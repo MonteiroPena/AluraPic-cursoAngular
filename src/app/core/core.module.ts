a25b8ebb71a426368bd888d933b5c528f01a1a67
MonteiroPena <dmpassis@gmail.com>
2020-07-06 14:55:02 -0300

    menu da aplicação

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { MenuModule } from "./../shared/components/menu/menu.module";
 import { RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
@@ -8,11 +9,19 @@ import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { RequestInterceptor } from "./auth/request.interceptor";
 import { FooterComponent } from "./footer/footer.component";
 import { AlertModule } from "../shared/components/alert/alert.module";
+import { ShowIfLoggedModule } from "../shared/directives/show-if-logged/show-if-logged.module";
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, RouterModule, AlertModule, LoadingModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    AlertModule,
+    LoadingModule,
+    MenuModule,
+    ShowIfLoggedModule,
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
--- src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -7,6 +7,8 @@ import { UserService } from "src/app/core/user/user.service";
   selector: "[showIfLogged]",
 })
 export class ShowIfLoggedDirective implements OnInit {
+  currentDisplay: string;
+
   constructor(
     private element: ElementRef<any>,
     // tslint:disable-next-line: deprecation
@@ -15,12 +17,24 @@ export class ShowIfLoggedDirective implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // tslint:disable-next-line: no-unused-expression
-    !this.userService.isLogged() &&
-      this.renderer.setElementStyle(
-        this.element.nativeElement,
-        "display",
-        "none"
-      );
+    this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+    this.userService.getUser().subscribe((user) => {
+      if (user) {
+        this.renderer.setElementStyle(
+          this.element.nativeElement,
+          "display",
+          this.currentDisplay
+        );
+      } else {
+        this.currentDisplay = getComputedStyle(
+          this.element.nativeElement
+        ).display;
+        this.renderer.setElementStyle(
+          this.element.nativeElement,
+          "display",
+          "none"
+        );
+      }
+    });
   }
 }
